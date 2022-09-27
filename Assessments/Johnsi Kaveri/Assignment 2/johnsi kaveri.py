5b796c8ef834422bd41cb38603d632fd79c46e98
ramyasendurraj <113965994+ramyasendurraj@users.noreply.github.com>
2022-09-27 14:04:52 +0530

    Add files via upload

--- Assessments/Johnsi Kaveri/Assignment 2/johnsi kaveri.py
+++ Assessments/Johnsi Kaveri/Assignment 2/johnsi kaveri.py
@@ -1,66 +1,13 @@
-Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+import random, time, winsound
 
-= RESTART: C:/Users/Ramya Sendurraj/AppData/Local/Programs/Python/Python310/ramya.py
-Tempareture is high
-Alert Temperature is high! !
-Traceback (most recent call last):
-  File "C:/Users/Ramya Sendurraj/AppData/Local/Programs/Python/Python310/ramya.py", line 9, in <module>
-    winsound.playSound("beep-07a.wav",winsound.SND_FILENAME)
-AttributeError: module 'winsound' has no attribute 'playSound'. Did you mean: 'PlaySound'?
 
-= RESTART: C:/Users/Ramya Sendurraj/AppData/Local/Programs/Python/Python310/ramya.py
-Tempareture is high
-Tempareture is high
-Alert Temperature is high! !
-Tempareture is high
-Alert Temperature is high! !
-Tempareture is high
-Tempareture is high
-Alert Temperature is high! !
-Alert Temperature is high! !
-Alert Temperature is high! !
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Alert Temperature is high! !
-Tempareture is high
-Alert Temperature is high! !
-Alert Temperature is high! !
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Alert Temperature is high! !
-Alert Temperature is high! !
-Alert Temperature is high! !
-Alert Temperature is high! !
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Alert Temperature is high! !
-Alert Temperature is high! !
-Tempareture is high
-Alert Temperature is high! !
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Alert Temperature is high! !
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Tempareture is high
-Alert Temperature is high! !
-Alert Temperature is high! !
-Tempareture is high
-Tempareture is high
-Alert Temperature is high! !
+
+while True:
+   temp = random.randint(1, 100)
+   if temp > 60:
+      print("Alert Temperature is high! !")
+      winsound.PlaySound("beep-07a.wav",winsound.SND_FILENAME)
+   else:
+      print("Tempareture is high")
+
+   time.sleep
